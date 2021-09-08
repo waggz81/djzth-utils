d8aa5b4b05eccd1845ed887846342d53f358247b
Josh Waggoner <joshwaggoner@gmail.com>
2021-09-08 02:40:10 -0500

    update listrole output format

--- commands/listrole.js
+++ commands/listrole.js
@@ -1,4 +1,5 @@
 const {config} = require('../config');
+const Table = require('easy-table')
 
 module.exports = {
     name: 'listrole',
@@ -15,8 +16,17 @@ module.exports = {
     async execute(interaction) {
         const Role = interaction.options.get('role').role;
         if (config.access_control.includes(interaction.channelId)) {
-            const Members = interaction.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => `${member.user.username}  (${member.user.tag})`);
-            interaction.reply({ content: `Users with ${Role.name}:\n${Members.join('\n')}`, ephemeral: true});
+            let list = [];
+            // noinspection EqualityComparisonWithCoercionJS
+            const Members = interaction.guild.members.cache.filter(member => member.roles.cache.find(role => role == Role));
+            Members.forEach ((member)=> {
+                list.push({ "Nickname": member.nickname || member.user.username, "Discord Tag": member.user.tag});
+            });
+            console.log(list)
+                //.map(member => { "Nickname": member.user.username, "Discord Tag": member.user.tag}));
+           // interaction.reply({ content: `Users with ${Role.name}:\n${Members.join('\n')}`, ephemeral: true});
+            interaction.reply({ content: `Users with \`@${Role.name}\`:\n\`\`\`${Table.print(list)}\`\`\``, ephemeral: true});
+
         }
         else {
             await interaction.reply({ content: "Command disabled in this channel", ephemeral: true });
