9a3576d361e635a28f80c132b62b6a60486132c8
waggz81 <joshwaggoner@gmail.com>
2024-02-13 21:07:29 -0600

    add gnomerreset command for sod phase 2

---
+++ commands/gnomerreset.ts
@@ -0,0 +1,35 @@
+import {config} from "../config";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {Colors, CommandInteraction, EmbedBuilder} from "discord.js";
+import {myLog} from "../index";
+import dayjs = require("dayjs");
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("gnomerreset")
+        .setDescription("When does Gnomeregan reset for Classic Season of Discovery?"),
+
+    async execute(interaction : CommandInteraction) {
+        let resettime = dayjs.unix(1708441200);
+        let lastreset = resettime;
+        const now = dayjs();
+        while (now > resettime) {
+            lastreset = resettime;
+            resettime = resettime.add(3,'d')
+        }
+        const embed = new EmbedBuilder({
+            title: `Gnomeregan resets:`,
+            description: ``,
+            color: Colors.Blue,
+        }).addFields(
+            { name: 'Next Reset:', value: `<t:${resettime.unix()}:F>\n(<t:${resettime.unix()}:R>)` },
+            { name: 'Last Reset:', value: `<t:${lastreset.unix()}:F>\n(<t:${lastreset.unix()}:R>)` },
+        )
+        await interaction.deferReply({ephemeral: false});
+        await interaction.editReply({embeds: [embed]})
+            .catch((err) => myLog(err))
+    }
+};
+
