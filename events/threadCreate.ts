301f20a99fccfc5a31a139b2b4887057ee7e4034
waggz81 <joshwaggoner@gmail.com>
2024-05-21 15:53:01 -0500

    removed bfd & gnomer reset commands added c&p embed for zth invites

--- events/threadCreate.ts
+++ events/threadCreate.ts
@@ -1,8 +1,45 @@
 import {config} from "../config";
-import {Events, ThreadAutoArchiveDuration} from "discord.js";
-import {client, myLog} from "../index";
+import {EmbedBuilder, Events, ThreadAutoArchiveDuration} from "discord.js";
+import {client, myLog, thisServer} from "../index";
 
 client.on(Events.ThreadCreate, async thread => {
+    if (thread.ownerId === '508391840525975553' && thread.name.startsWith('zth-')) {
+        let nameAndRealm = '';
+        let submitterDiscordId = '';
+        console.log("zth invite ticket thread created " + thread.id)
+        setTimeout(() => {
+            thread.messages.fetch().then(messages => {
+                messages.forEach(message => {
+                   message.embeds.forEach(thisEmbed => {
+                       if (thisEmbed.fields.length === 5) {
+                           nameAndRealm = `${thisEmbed.fields[0].value}-${thisEmbed.fields[1].value}`;
+                       }
+                       else if (thisEmbed.description){
+                           const re = /<@(.*)>/;
+                           const submitterId = thisEmbed.description.match(re);
+                           if (submitterId) {
+                               submitterId.forEach(match => {
+                                   if (thread.members.cache.has(match)) {
+                                       submitterDiscordId = match;
+                                   }
+                               })
+                           }
+                       }
+
+                   })
+                });
+                const notesEmbed = new EmbedBuilder()
+                    .setTitle('Copy & Paste')
+                    .addFields(
+                        { name: 'Character', value: `\`${nameAndRealm}\`` },
+                        { name: 'Guild Note', value: `\`[XFa:${thisServer.members.cache.get(submitterDiscordId)?.nickname}]\``},
+                        { name: 'Officer Note', value: `\`<@${submitterDiscordId}>\``}
+                    );
+                thread.send({embeds: [notesEmbed]}).catch(err => console.log(err));
+            });
+        }, 1000 * 3);
+    }
+    // dj application auto mentions
     const forums = config.forum_post_auto_mention_roles;
     if (thread.parentId) {
         if (forums[thread.parentId]) {
